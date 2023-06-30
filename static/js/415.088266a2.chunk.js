39fd78c43680a159b99d101aee7a0c2ee7ddc5cb
d1mkn <46346986+d1mkn@users.noreply.github.com>
2023-06-30 23:55:48 +0000

    Deploying to gh-pages from @ d1mkn/phonebook@dbc27fab55bc2e720e302b39bcac91857b3c8ec0 🚀

---
+++ static/js/415.088266a2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkphonebook=self.webpackChunkphonebook||[]).push([[415],{5415:function(e,n,o){o.r(n),o.d(n,{default:function(){return c}});var u=o(184);function c(){return(0,u.jsx)("div",{children:(0,u.jsx)("h2",{children:"Phonebook welcome page"})})}}}]);
+//# sourceMappingURL=415.088266a2.chunk.js.map
